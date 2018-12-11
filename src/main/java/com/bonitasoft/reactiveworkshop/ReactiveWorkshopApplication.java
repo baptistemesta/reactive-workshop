11a574d00dc83d767e693e8ce7061f75ec8c7498
Baptiste Mesta <baptiste.mesta@gmail.com>
2018-12-11 13:33:06 +0100

    init project

---
+++ src/main/java/com/bonitasoft/reactiveworkshop/ReactiveWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.bonitasoft.reactiveworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWorkshopApplication.class, args);
+	}
+}
