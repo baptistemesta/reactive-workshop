fc37b7636eadaac470e2fbbd979b9d4fd1ca2b99
tbouffard <thomas.bouffard@bonitasoft.com>
2018-12-14 02:02:11 +0100

    wip: try to block with reactor - not working

--- backend/src/main/java/com/bonitasoft/reactiveworkshop/repository/CommentsClient.java
+++ backend/src/main/java/com/bonitasoft/reactiveworkshop/repository/CommentsClient.java
@@ -1,18 +1,52 @@
 package com.bonitasoft.reactiveworkshop.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.bonitasoft.reactiveworkshop.domain.ArtistWithComments;
 import com.bonitasoft.reactiveworkshop.domain.ArtistWithComments.Comment;
 import com.bonitasoft.reactiveworkshop.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Repository
+@Slf4j
 public class CommentsClient {
 
-    public List<Comment> getComments(String artistId) throws NotFoundException {
+    private final WebClient webClient;
+    private final RestTemplate restTemplate;
+    // TODO utiliser la prop de l'application
+    private final String baseUrl = "http://localhost:3004";
 
-        return null;
+    public CommentsClient(WebClient.Builder webClientBuilder, RestTemplateBuilder restTemplateBuilder) {
+        // highly inspired from https://grokonez.com/spring-framework/spring-webflux/spring-webclient-spring-webflux-springboot-2
+        this.webClient = webClientBuilder.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(baseUrl).build();
+
+        this.restTemplate = restTemplateBuilder.rootUri(baseUrl).build();
+    }
+
+    public List<Comment> getComments(String artistId) throws NotFoundException {
+        log.info("Retrieving comments for artist {}", artistId);
+//        ParameterizedTypeReference<List<Comment>> commentListypeRef = new ParameterizedTypeReference<List<Comment>>() {};
+//        restTemplate.exchange(String.format("//%s/comments", artistId), HttpMethod.GET, commentListypeRef);
+
+        List<Comment> comments = new ArrayList<>();
+
+        this.webClient.get().uri("/comments/{artistId}/last10", artistId)
+                .retrieve().bodyToFlux(Comment.class)
+                .subscribe(c -> {
+                    log.info("Retrieved comment {}", c);
+//                    comments.add(c);
+                });
+        // wrong way to do, block is not allow in reactor 3.2+
+//                .collectList().block();
+        log.info("Found {} comments", comments.size());
+        return comments;
     }
 
 }
