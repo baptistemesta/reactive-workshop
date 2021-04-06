3841d7d2be1e5f89b5b1748fc65b0c6ddd1fc50b
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-04-06 16:46:03 +0200

    chore: cleanup

--- external-service/src/main/java/com/bonitasoft/reactiveworkshop/external/CommentGenerator.java
+++ external-service/src/main/java/com/bonitasoft/reactiveworkshop/external/CommentGenerator.java
@@ -7,7 +7,6 @@ import static java.lang.Math.round;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
